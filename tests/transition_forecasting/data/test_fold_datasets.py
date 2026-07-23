6930547effc691c00d5170f5abb891f7fbea77cb
Claudia Friedsam <claudia.friedsam@gmail.com>
2026-07-23 00:24:31 -0400

    Test conservative positive maturity fallback

--- tests/transition_forecasting/data/test_fold_datasets.py
+++ tests/transition_forecasting/data/test_fold_datasets.py
@@ -39,4 +39,46 @@ def test_positive_label_end_is_onset_plus_persistence_window_minus_one() -> None
     enriched = _attach_positive_label_end_dates(manifest, {"IDX": series})
 
     assert pd.Timestamp(enriched.loc[0, "label_end_date"]) == dates[40 + M - 1]
+    assert enriched.loc[0, "label_end_date_source"] == "exact_trading_row"
     assert pd.Timestamp(enriched.loc[1, "label_end_date"]) == dates[30]
+    assert enriched.loc[1, "label_end_date_source"] == "target_end_nonpositive"
+
+
+def test_positive_label_end_uses_catalogue_history_end_when_packaged_series_is_short() -> None:
+    dates = pd.bdate_range("2000-01-03", periods=50)
+    series = pd.Series(range(len(dates)), index=dates, dtype=float)
+    onset = dates[45]
+    target_end = dates[49]
+    source_history_end = pd.Timestamp("2000-04-30")
+    manifest = pd.DataFrame(
+        [
+            {
+                "sample_id": "P_EDGE",
+                "label": 1,
+                "index": "IDX",
+                "event_onset": onset,
+                "target_end_date": target_end,
+            }
+        ]
+    )
+    catalogue = pd.DataFrame(
+        [
+            {
+                "index": "IDX",
+                "onset_date": onset,
+                "history_end": source_history_end,
+            }
+        ]
+    )
+
+    enriched = _attach_positive_label_end_dates(
+        manifest,
+        {"IDX": series},
+        catalogue,
+    )
+
+    assert pd.Timestamp(enriched.loc[0, "label_end_date"]) == source_history_end
+    assert (
+        enriched.loc[0, "label_end_date_source"]
+        == "catalogue_history_end_conservative"
+    )
